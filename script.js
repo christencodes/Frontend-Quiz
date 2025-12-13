2b3a93ced23039a84ef9be77eb3c0bac4f35f9d8
christenally <13616443+Christenally@users.noreply.github.com>
2025-12-12 21:40:08 -0600

    Creating functions to populate JSON data

--- script.js
+++ script.js
@@ -9,9 +9,22 @@ a. review how to access JSOn file
 
 question: make it state based?
 */
-// let jsonContainer;
+let jsonContainer;
 // SELECTORS
+const leftCard = document.querySelector(".left-card");
 const rightCard = document.querySelector(".right-card");
+
+const leftInfo = leftCard.querySelector(".left-info");
+const rightInfo = rightCard.querySelector(".right-info");
+
+//A B C D...0, 1 , 2 , 3
+
+const question0 = document.getElementById("0");
+const question1 = document.getElementById("1");
+const question2 = document.getElementById("2");
+const question3 = document.getElementById("3");
+
+const questionsArray = [question0, question1, question2, question3];
 
 // EVENT LISTENERS
 rightCard.addEventListener("click", (e) => {
@@ -19,28 +32,28 @@ rightCard.addEventListener("click", (e) => {
   //if we click and it's a certain id
 
   categoryState.state(e.target.id);
+  quizTracker.clearMain();
   //? change the current state to
 });
 
 // FUNCTIONS
 //function to get JSON data
 
-// async function getQuizData() {
-//   const requestURL = "data.json";
-//   const request = new Request(requestURL);
+async function getQuizData() {
+  const requestURL = "data.json";
+  const request = new Request(requestURL);
 
-//   const response = await fetch(request);
-//   const dataText = await response.text();
+  const response = await fetch(request);
+  const dataText = await response.text();
 
-//   const data = JSON.parse(dataText);
-//   return data;
-// }
+  const data = JSON.parse(dataText);
+  return data;
+}
 
-// getQuizData().then((result) => {
-//   jsonContainer = result;
-// });
+getQuizData().then((result) => {
+  jsonContainer = result;
+});
 
-let jsonContainer;
 fetch("data.json")
   .then((result) => result.text())
   .then((result) => {
@@ -53,14 +66,19 @@ const categoryState = {
     for (const state in this) {
       if (state == id) {
         this[state] = true;
-      } else if (state == "state") {
+      } else if (state == "state" || state == "currentActiveState") {
         continue;
       } else {
         this[state] = false;
       }
     }
-    //this.[id] -> true - change the others to false
-    //I want this to loop through the values and determine which one is true and make the others false
+  },
+  currentActiveState: function () {
+    for (const state in this) {
+      if (this[state] == true) {
+        return state;
+      }
+    }
   },
   main: false,
   html: false,
@@ -71,7 +89,7 @@ const categoryState = {
 };
 
 const quizTracker = {
-  state: "",
+  state: () => categoryState.currentActiveState(),
   stateIcon: "",
   totalQuestions: 0,
   currentScore: 0,
@@ -81,4 +99,30 @@ const quizTracker = {
   currentQuestionAnswer: "",
   currentUserAnswer: "",
   answerSubmitted: false,
+  currentQuestions: "",
+
+  clearMain: function () {
+    leftInfo.classList.add("hidden");
+    rightInfo.classList.add("hidden");
+  },
+
+  populateQuestions: function () {
+    jsonContainer.quizzes.array.forEach((element) => {
+      if (jsonContainer.quizzes[element].title == this.state()) {
+        for (
+          let question = 0;
+          question <
+          jsonContainer.quizzes[element].questions[this.currentQuestion].options
+            .length;
+          question++
+        ) {
+          questionsArray[question].textContent =
+            jsonContainer.quizzes[element].questions[
+              this.currentQuestion
+            ].options[question];
+        }
+      }
+    });
+  },
+  // keep track of everything populate the left and right side
 };
